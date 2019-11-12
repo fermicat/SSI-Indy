9bb3c05ccd5ed08e48c7164347e96d487da38916
Zicheng Wang <zicheng_wang2015@hotmail.com>
2019-11-12 01:28:49 -0600

    update the main.py

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# structure
+
+`.src` contains the operations with DIDs, Verkey, Credential, Proof and exchange with wallet and ledger. Done.
+`produce.py` contains the supply chain implementation of the architecture. To be finished.
---
+++ produce.py
@@ -0,0 +1,96 @@
+import time
+
+from indy import anoncreds, crypto, did, ledger, pool, wallet
+
+import json
+import logging
+from typing import Optional
+
+from indy.error import ErrorCode, IndyError
+
+from src.utils import get_pool_genesis_txn_path, run_coroutine, PROTOCOL_VERSION
+import subprocess
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+async def run():
+
+    """
+    *****************************MAIN************************
+    go through the typical process
+    """
+
+    logger.info("=== Getting Trust Anchor credentials for Producer, QA Team, Distributor  ==")
+
+    logger.info("== Getting Trust Anchor credentials - Producer Onboarding  ==")
+
+
+
+async def onboarding(pool_handle, _from, from_wallet, from_did, to, to_wallet: Optional[str], to_wallet_config: str,
+                     to_wallet_credentials: str):
+    logger.info("\"{}\" -> Create and store in Wallet \"{} {}\" DID".format(_from, _from, to))
+    (from_to_did, from_to_key) = await did.create_and_store_my_did(from_wallet, "{}")
+
+    logger.info("\"{}\" -> Send Nym to Ledger for \"{} {}\" DID".format(_from, _from, to))
+    await send_nym(pool_handle, from_wallet, from_did, from_to_did, from_to_key, None)
+
+    logger.info("\"{}\" -> Send connection request to {} with \"{} {}\" DID and nonce".format(_from, to, _from, to))
+    connection_request = {
+        'did': from_to_did,
+        'nonce': 123456789
+    }
+
+    if not to_wallet:
+        logger.info("\"{}\" -> Create wallet".format(to))
+        try:
+            await wallet.create_wallet(to_wallet_config, to_wallet_credentials)
+        except IndyError as ex:
+            if ex.error_code == ErrorCode.PoolLedgerConfigAlreadyExistsError:
+                pass
+        to_wallet = await wallet.open_wallet(to_wallet_config, to_wallet_credentials)
+
+    logger.info("\"{}\" -> Create and store in Wallet \"{} {}\" DID".format(to, to, _from))
+    (to_from_did, to_from_key) = await did.create_and_store_my_did(to_wallet, "{}")
+
+    logger.info("\"{}\" -> Get key for did from \"{}\" connection request".format(to, _from))
+    from_to_verkey = await did.key_for_did(pool_handle, to_wallet, connection_request['did'])
+
+    logger.info("\"{}\" -> Anoncrypt connection response for \"{}\" with \"{} {}\" DID, verkey and nonce"
+                .format(to, _from, to, _from))
+    connection_response = json.dumps({
+        'did': to_from_did,
+        'verkey': to_from_key,
+        'nonce': connection_request['nonce']
+    })
+    anoncrypted_connection_response = await crypto.anon_crypt(from_to_verkey, connection_response.encode('utf-8'))
+
+    logger.info("\"{}\" -> Send anoncrypted connection response to \"{}\"".format(to, _from))
+
+    logger.info("\"{}\" -> Anondecrypt connection response from \"{}\"".format(_from, to))
+    decrypted_connection_response = \
+        json.loads((await crypto.anon_decrypt(from_wallet, from_to_key,
+                                              anoncrypted_connection_response)).decode("utf-8"))
+
+    logger.info("\"{}\" -> Authenticates \"{}\" by comparision of Nonce".format(_from, to))
+    assert connection_request['nonce'] == decrypted_connection_response['nonce']
+
+    logger.info("\"{}\" -> Send Nym to Ledger for \"{} {}\" DID".format(_from, to, _from))
+    await send_nym(pool_handle, from_wallet, from_did, to_from_did, to_from_key, None)
+
+    return to_wallet, from_to_key, to_from_did, to_from_key, decrypted_connection_response
+
+
+
+async def get_verinym(pool_handle, _from, from_wallet, from_did, from_to_key,
+                      to, to_wallet, to_from_did, to_from_key, role):
+    
+    return
+
+async def send_nym(pool_handle, wallet_handle, _did, new_did, new_key, role):
+    nym_request = await ledger.build_nym_request(_did, new_did, new_key, None, role)
+    await ledger.sign_and_submit_request(pool_handle, wallet_handle, _did, nym_request)
+
+
+async def send_schema(pool_handle, wallet_handle, _did, schema):
+    schema_request = await ledger.build_schema_request(_did, schema)
+    await ledger.sign_and_submit_request(pool_handle, wallet_handle, _did, schema_request)
